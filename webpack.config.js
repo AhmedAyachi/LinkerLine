947eaca3e6530b103c13f1694caf0a485b21fc2a
AhmedAyachi <aayachi032@gmail.com>
2025-04-28 18:11:53 +0100

    fix: lines not drawn on a gecko env

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ module.exports={
     mode:"production",
     entry:{
         linkerline:entryFilePath,
-        ["linkerline-"+pkgjson.version]:entryFilePath,
     },
     output:{
         library:{
